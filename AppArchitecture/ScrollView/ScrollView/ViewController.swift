d565eca826214dcf6b03940b8ecc884af07e959d
NESTOR IVAN MARTINEZ OSTOA <nestor.martinez.98@hotmail.com>
2019-03-18 21:15:44 -0600

    Added UIScrollView example

---
+++ AppArchitecture/ScrollView/ScrollView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollView
+//
+//  Created by Néstor Iván on 3/18/19.
+//  Copyright © 2019 Néstor Iván. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
